f23ec71987be21fac8f85f2a3f30cdc0c1452b52
cryptomagnum <jorge_vankov@hotmail.com>
2024-03-07 15:01:34 +0100

    Añado la vista index y dentro todo el contenido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './views/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes =
+    [
+        { path: '', redirectTo: 'index', pathMatch: 'full' },
+        { path: 'index', component: IndexComponent }
+    ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,41 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  public styleOne: boolean = true;
+  public styleTwo: boolean = false;
+  public styleThree: boolean = false;
+  public styleFour: boolean = false;
+  public styleFive: boolean = false;
 
+  public activeStyle(style: number): void {
+    this.styleOne = false;
+    this.styleTwo = false;
+    this.styleThree = false;
+    this.styleFour = false;
+    this.styleFive = false;
+    if (style === 1) {
+      this.styleOne = true;
+    }
+    if (style === 2) {
+      this.styleTwo = true;
+    }
+    if (style === 3) {
+      this.styleThree = true;
+    }
+    if (style === 4) {
+      this.styleFour = true;
+    }
+    if (style === 5) {
+      this.styleFive = true;
+    }
+  }
 }
