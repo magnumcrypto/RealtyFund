488439dcbe1d6a9ce214ae5420dfa41f53433659
cryptomagnum <jorge_vankov@hotmail.com>
2024-03-19 12:55:02 +0100

    Genero el componente card para la vista sales. Añado imagenes y un directorio para ellos Soluciono algunos errores

--- src/app/views/sales/sales.component.ts
+++ src/app/views/sales/sales.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-sales',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './sales.component.html',
   styleUrl: './sales.component.css'
 })
 export class SalesComponent {
-
+  public cardImage1: string = '../../../assets/house_img/gallery-2681238_1280.jpg';
+  public cardImage2: string = '../../../assets/house_img/house-1477041_1280.jpg';
+  public cardImage3: string = '../../../assets/house_img/house-1836070_1280.jpg';
 }
